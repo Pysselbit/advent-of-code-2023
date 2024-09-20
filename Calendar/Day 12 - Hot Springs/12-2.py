22be795e73dd707f60960e0567baf23fc3562fc6
Joakim <joakimrasmuson@gmail.com>
2024-09-20 12:42:31 +0200

    Solve Day 12: Hot Springs

---
+++ Calendar/Day 12 - Hot Springs/12-2.py
@@ -0,0 +1,59 @@
+# Advent of Code 2023
+# Day 12-2: Hot Springs
+
+import re
+
+OPERATIONAL = "."
+DAMAGED = "#"
+UNKNOWN = "?"
+
+valid_permutations_cache = {}
+
+
+class SpringSet:
+    def __init__(self, springs, groups):
+        self.springs = springs
+        self.groups = groups
+
+
+def parse_input(path, times):
+    with open(path) as file:
+        spring_sets = []
+
+        for line in [x.strip().split() for x in file.readlines()]:
+            springs = "?".join([line[0]] * times)
+            groups = tuple(int(x) for x in re.findall(r"\d+", line[1])) * times
+            spring_sets.append(SpringSet(springs, groups))
+
+        return spring_sets
+
+
+def get_valid_permutations(springs, groups):
+    if springs == "":
+        return 1 if len(groups) == 0 else 0
+
+    if len(groups) == 0:
+        return 1 if DAMAGED not in springs else 0
+
+    key = (springs, groups)
+    if key in valid_permutations_cache:
+        return valid_permutations_cache[key]
+
+    sum_valid_permutations = 0
+
+    if springs[0] in ".?":
+        sum_valid_permutations += get_valid_permutations(springs[1:], groups)
+
+    if springs[0] in "#?":
+        if (groups[0] <= len(springs) and OPERATIONAL not in springs[:groups[0]] and
+                (len(springs) == groups[0] or springs[groups[0]] != DAMAGED)):
+            sum_valid_permutations += get_valid_permutations(springs[groups[0] + 1:], groups[1:])
+
+    valid_permutations_cache[key] = sum_valid_permutations
+    return sum_valid_permutations
+
+
+spring_sets = parse_input("input.txt", 5)
+sum_valid_permutations = sum([get_valid_permutations(x.springs, x.groups) for x in spring_sets])
+
+print(sum_valid_permutations)
